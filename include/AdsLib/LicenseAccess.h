f5fdaa8dd58e3bbc7fdeb6b6b5ca7077bdf643f0
Dshieff <107025792+Dshieff@users.noreply.github.com>
2022-08-09 15:33:11 +1200

    Added files

---
+++ include/AdsLib/LicenseAccess.h
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+/**
+   Copyright (c) 2021 - 2022 Beckhoff Automation GmbH & Co. KG
+ */
+
+#pragma once
+
+#include "AdsDevice.h"
+
+namespace bhf
+{
+namespace ads
+{
+struct LicenseAccess {
+    LicenseAccess(const std::string& gw, AmsNetId netid, uint16_t port);
+    bool ShowPlatformId(std::ostream& os) const;
+    bool ShowSystemId(std::ostream& os) const;
+    bool ShowVolumeNo(std::ostream& os) const;
+private:
+    AdsDevice device;
+};
+}
+}
